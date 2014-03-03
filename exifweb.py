e8f8d9a6c29e828761ec211c616e3bdab49b3ec1
arrowed <arrowed@arrowed.org>
2014-03-04 00:28:03 +1100

    Added web methods

--- exifweb.py
+++ exifweb.py
@@ -1,4 +1,5 @@
 from flask import Flask, Response, abort, flash, redirect, render_template, request, url_for
+import json
 
 import redis_helper as redis_conn
 
@@ -15,6 +16,7 @@ def index():
     return render_template('main.html', next=url_for('index'))
 
 
+
 @app.route('/add', methods=['GET', 'POST'])
 def add_folder():
     if request.form['directory']:
@@ -28,6 +30,23 @@ def add_folder():
         return redirect(request.form['next'])
 
     return render_template('main.html', next=url_for('index'))
+
+@app.route('/updatestatus', methods=['GET', 'POST'])
+def update():
+    if request.form['directory'] and request.form['processed'] and request.form['total']:
+
+        directory = request.form['directory']
+        jobs = redis_conn._add_job_to_queue('jobs', directory )
+
+
+    return render_template('main.html', next=url_for('index'))
+
+
+@app.route('/get', methods=['GET'])
+def get_next():
+
+    jobs = redis_conn._get_job_from_queue('jobs')
+    return json.dumps({'directory': jobs})
 
 
 def find_nth(number):
--- redis_helper.py
+++ redis_helper.py
@@ -8,4 +8,15 @@ def _add_job_to_queue(queue, job):
     key = 'queue:%s' % queue
     conn.sadd(key, job)
 
-    return conn.scard(queue)
+    return conn.scard(key)
+
+def _get_job_from_queue(queue):
+    key = 'queue:%s' % queue
+    return conn.srandmember(key)
+
+
+def _processor_stream(red):
+    pubsub = conn.pubsub()
+    pubsub.subscribe('processor_stream')
+    for message in pubsub.listen():
+        yield 'retry: 50000\ndata: %s\n\n' % message['data']
